69c6cca1a7b9d49210955614fe9fac21dcdc6d06
Richard Chalk <barmybrit@gmail.com>
2023-06-27 17:32:40 +0200

    Final v1

---
+++ Project1/Book.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    public class Book
+    {
+        private string title;
+        private string author;
+        private int pages;
+        private int wordCount;
+
+        // Samtliga instanser av datatyp Book dela på denna variabel!
+        private static int totalBooks;
+        
+        public Book(string title, string author)
+        {
+            this.title = title;
+            this.author = author;
+            totalBooks += 1;
+        }
+
+        public Book(string title, string author, int pages, int wordCount)
+        {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.wordCount = wordCount;
+            totalBooks += 1;
+        }
+
+        public string GetTitle()
+        {
+            return title;
+        }
+        public void SetTitle(string title)
+        {
+            this.title = title;
+        }
+        public void AssignWordCountFromText(string text)
+        {
+            wordCount = text.Split(' ').Length;
+        }
+
+        public static int GetTotalBooks()
+        {
+            return totalBooks;
+        }
+
+        // Scope example
+        public void DoSomething()
+        {
+            int a = 0;
+            for (int x = 0; x < 10; x++)
+            {
+                // x only exists within this for loop!
+                Console.WriteLine(x);
+            }
+               
+            // This won't compile--the variable 'x' is "out of scope".
+            // x = -5; 
+        }
+    }
+}
--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace MultiProject
+﻿using Project1;
+
+namespace MultiProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, from project 1!");
+
+            Book book = new Book("Harry Potter", "J.K. Rowling");
+            // Changed my mind. Let's use the full name.
+            book.SetTitle("Harry Potter and the Half-Blood Prince.");
+            // Now I forgot. What was the title again?
+            Console.WriteLine(book.GetTitle());
+
+            // Calling a "static" method from Book
+            Console.WriteLine(Book.GetTotalBooks());
+
+
         }
     }
 }
