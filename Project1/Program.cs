e9e700562731e75c752862e29e216a5b3c0a4cdd
Richard Chalk <barmybrit@gmail.com>
2023-06-27 11:18:33 +0200

    Add project files.

---
+++ Project1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, from project 1!");
+        }
+    }
+}
